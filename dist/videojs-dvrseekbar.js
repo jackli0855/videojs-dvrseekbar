8cb79d7836b8f68d316a39f700e1d377f5f0036c
Sergey Gromkov <sgromkov@gmail.com>
2017-09-20 13:07:53 +0300

    Added "dist" folder to git

--- dist/videojs-dvrseekbar.js
+++ dist/videojs-dvrseekbar.js
@@ -1,7 +1,7 @@
 /**
  * videojs-dvrseekbar
  * @version 0.2.6
- * @copyright 2016 ToolBox-tve
+ * @copyright 2017 ToolBox-tve
  * @license Apache-2.0
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.videojsDvrseekbar = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -21,6 +21,56 @@ var _videoJs2 = _interopRequireDefault(_videoJs);
 // Default options for the plugin.
 var defaults = {
   startTime: 0
+};
+
+var SeekBar = _videoJs2['default'].getComponent('SeekBar');
+
+SeekBar.prototype.dvrTotalTime = function (player) {
+  var time = player.seekable();
+
+  return time && time.length ? time.end(0) - time.start(0) : 0;
+};
+
+SeekBar.prototype.handleMouseMove = function (e) {
+  var bufferedTime = undefined;
+  var newTime = undefined;
+
+  bufferedTime = newTime = this.player_.seekable();
+
+  if (bufferedTime && bufferedTime.length) {
+    var progress = this.calculateDistance(e) * this.dvrTotalTime(this.player_);
+
+    newTime = bufferedTime.start(0) + progress;
+    for (; newTime >= bufferedTime.end(0);) {
+      newTime -= 0.1;
+    }
+
+    this.player_.currentTime(newTime);
+  }
+};
+
+SeekBar.prototype.updateAriaAttributes = function () {
+  var seekableRanges = this.player_.seekable() || [];
+
+  if (seekableRanges.length) {
+    var lastSeekableTime = seekableRanges.end(0);
+    var cachedCTime = this.player_.getCache().currentTime;
+    var currentTime = this.player_.scrubbing ? cachedCTime : this.player_.currentTime();
+    var timeToLastSeekable = undefined;
+
+    // Get difference between last seekable moment and current time
+    timeToLastSeekable = lastSeekableTime - currentTime;
+    if (timeToLastSeekable < 0) {
+      timeToLastSeekable = 0;
+    }
+
+    // Update current time control
+    var formattedTime = _videoJs2['default'].formatTime(timeToLastSeekable, lastSeekableTime);
+    var formattedPercentage = Math.round(100 * this.getPercent(), 2);
+
+    this.el_.setAttribute('aria-valuenow', formattedPercentage);
+    this.el_.setAttribute('aria-valuetext', (currentTime ? '' : '-') + formattedTime);
+  }
 };
 
 /**
@@ -43,8 +93,8 @@ var onPlayerReady = function onPlayerReady(player, options) {
   }
 
   // ADD Live Button:
-  var btnLiveEl = document.createElement('div'),
-      newLink = document.createElement('button');
+  var btnLiveEl = document.createElement('div');
+  var newLink = document.createElement('a');
 
   btnLiveEl.className = 'vjs-live-button vjs-control';
 
@@ -57,7 +107,6 @@ var onPlayerReady = function onPlayerReady(player, options) {
 
   var clickHandler = function clickHandler(e) {
     player.currentTime(player.seekable().end(0));
-
     player.play();
   };
 
@@ -73,8 +122,8 @@ var onPlayerReady = function onPlayerReady(player, options) {
 
   btnLiveEl.appendChild(newLink);
 
-  var controlBar = document.getElementsByClassName('vjs-control-bar')[0],
-      insertBeforeNode = document.getElementsByClassName('vjs-progress-control')[0];
+  var controlBar = document.getElementsByClassName('vjs-control-bar')[0];
+  var insertBeforeNode = document.getElementsByClassName('vjs-progress-control')[0];
 
   controlBar.insertBefore(btnLiveEl, insertBeforeNode);
 
@@ -85,21 +134,19 @@ var onTimeUpdate = function onTimeUpdate(player, e) {
   var time = player.seekable();
   var btnLiveEl = document.getElementById('liveButton');
 
-  if (!time.length) {
+  // When any tech is disposed videojs will trigger a 'timeupdate' event
+  // when calling stopTrackingCurrentTime(). If the tech does not have
+  // a seekable() method, time will be undefined
+  if (!time || !time.length) {
     return;
   }
 
-  /*let time1 = time && time.length ? time.end(0) - time.start(0) : 0;
-   if(time1 > 0) {
-    player.duration(time1 + 2);
-  }
-  */
+  player.duration(player.seekable().end(0));
 
   if (time.end(0) - player.currentTime() < 30) {
-
-    btnLiveEl.className = 'vjs-live-label onair';
+    btnLiveEl.className = 'label onair';
   } else {
-    btnLiveEl.className = 'vjs-live-label';
+    btnLiveEl.className = 'label';
   }
 
   player.duration(player.seekable().end(0));
@@ -119,36 +166,6 @@ var onTimeUpdate = function onTimeUpdate(player, e) {
  */
 var dvrseekbar = function dvrseekbar(options) {
   var _this = this;
-
-  var player = this;
-  var SeekBar = _videoJs2['default'].getComponent('SeekBar');
-
-  SeekBar.prototype.dvrTotalTime = function (player) {
-    var time = player.seekable();
-
-    return time && time.length ? time.end(0) - time.start(0) : 0;
-  };
-
-  SeekBar.prototype.handleMouseMove = function (e) {
-    var bufferedTime = undefined,
-        newTime = undefined;
-
-    bufferedTime = newTime = this.player_.seekable();
-
-    if (bufferedTime && bufferedTime.length) {
-      for (newTime = bufferedTime.start(0) + this.calculateDistance(e) * this.dvrTotalTime(this.player_); newTime >= bufferedTime.end(0);) newTime -= .1;
-
-      this.player_.currentTime(newTime);
-    }
-  };
-
-  SeekBar.prototype.updateAriaAttributes = function () {
-    var a = undefined,
-        c = undefined,
-        d = this.player_.seekable();
-
-    d && d.length && (a = this.player_.scrubbing ? this.player_.getCache().currentTime : this.player_.currentTime(), c = d.end(0) - a, c = 0 > c ? 0 : c, this.el_.setAttribute('aria-valuenow', Math.round(100 * this.getPercent(), 2)), this.el_.setAttribute('aria-valuetext', (0 === a ? "" : "-") + _videoJs2['default'].formatTime(c, d.end(0))));
-  };
 
   if (!options) {
     options = defaults;
